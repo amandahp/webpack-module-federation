1303e29043a3d5b59782c13276e5631779681f63
amandahp <amandahoffmann96@gmail.com>
2021-03-24 18:46:37 -0300

    feat: create app Home

---
+++ home/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports={
+  entry: './src/index.js',
+  output:{
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'http://localhost:9005/'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    index: 'index.html',
+    port: 9005,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"]
+  },
+  module: {    
+    rules:[
+      {
+          test: /\.jsx?$/,
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [require.resolve("@babel/preset-react")]
+          }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+      title: 'Home'
+    })
+  ]
+
+}
